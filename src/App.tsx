3afcc704a569a86c07d7674691a6ba27d3ddf87c
Tannaz Shirzadi <tia.shr97@gmail.com>
2023-11-07 15:26:56 +0300

    feat: init styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import "./App.css";
-
 function App() {
-  return <div className="text-3xl">App</div>;
+  return (
+    <main className="flex min-h-screen flex-col">
+      <div>hero section</div>
+      <div
+        className="flex flex-col
+      xl:w-[1200px] lg:w-[1000px] lg:mx-auto w-full px-4"
+      >
+        main section
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#222831",
+      dark: "#393E46",
+      teal: "#00ADB5",
+      grey: "#eeeeee",
+    },
     extend: {},
   },
   plugins: [],
